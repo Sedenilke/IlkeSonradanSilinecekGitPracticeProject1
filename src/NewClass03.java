94a5cd84275dcf142a31182a72b6335f0445afb1
Sedenilke <sedenilke@gmail.com>
2022-12-19 13:19:02 -0800

    I added NewClass05 and I modified NewClass03

--- src/NewClass03.java
+++ src/NewClass03.java
@@ -1,5 +1,6 @@
 public class NewClass03 {
     public static void main(String[] args) {
         System.out.println("Here is my another class" );
+        System.out.println("I modified this class");
     }
 }
---
+++ src/NewClass05.java
@@ -0,0 +1,5 @@
+public class NewClass05 {
+    public static void main(String[] args) {
+        System.out.println("I created this class after creating a new project and I have not push it to remote repository");
+    }
+}
