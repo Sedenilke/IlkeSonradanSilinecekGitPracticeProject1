d7e541ec42f5d075af6159a4df89e9a83ab24adc
Sedenilke <sedenilke@gmail.com>
2022-12-19 13:00:10 -0800

    I added NewClass03 and I also modified NewClass02

--- src/NewClass.java
+++ src/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Here is my new class");
         System.out.println("I added new things in it");
+        System.out.println("I aslo added some new stuff into this class");
     }
 }
---
+++ src/NewClass03.java
@@ -0,0 +1,5 @@
+public class NewClass03 {
+    public static void main(String[] args) {
+        System.out.println("Here is my another class" );
+    }
+}
