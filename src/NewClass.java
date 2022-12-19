8318e1c880ddb76e0396f434e27b7a6e439dc3bb
Sedenilke <sedenilke@gmail.com>
2022-12-19 12:57:22 -0800

    NewClass02 has been added

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,5 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Here is my new class");
+        System.out.println("I added new things in it");
     }
 }
---
+++ src/NewClass02.java
@@ -0,0 +1,5 @@
+public class NewClass02 {
+    public static void main(String[] args) {
+        System.out.println("Here is my new second class");
+    }
+}
